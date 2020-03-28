375d3a1ef6a11806f4b64cc661d5f2bfe2bc4688
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-03-27 17:32:37 -0700

    fix loader bug

--- src/vivarium_csu_zenon/data/loader.py
+++ src/vivarium_csu_zenon/data/loader.py
@@ -46,6 +46,8 @@ def get_data(lookup_key: str, location: str) -> pd.DataFrame:
         project_globals.POPULATION.DEMOGRAPHY: load_demographic_dimensions,
         project_globals.POPULATION.TMRLE: load_theoretical_minimum_risk_life_expectancy,
         project_globals.POPULATION.ACMR: load_standard_data,
+        project_globals.POPULATION.PROPENSITY_CORRELATION_DATA: load_propensity_correlation_data,
+        project_globals.POPULATION.JOINT_PAF_DATA: load_joint_paf_data,
 
         project_globals.IHD.ACUTE_MI_PREVALENCE: load_ihd_prevalence,
         project_globals.IHD.POST_MI_PREVALENCE: load_ihd_prevalence,
@@ -122,9 +124,6 @@ def get_data(lookup_key: str, location: str) -> pd.DataFrame:
         project_globals.IKF.CSMR: load_ckd_standard_data,
         project_globals.IKF.EMR: load_ckd_standard_data,
         project_globals.IKF.PAF: load_ikf_paf,
-
-        project_globals.PROPENSITY_CORRELATION_DATA: load_propensity_correlation_data,
-        project_globals.JOINT_PAF_DATA: load_joint_paf_data,
     }
     return mapping[lookup_key](lookup_key, location)
 
