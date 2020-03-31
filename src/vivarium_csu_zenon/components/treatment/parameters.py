f4bf4a38ade82682d75d03ec17f8db0a48adf93e
James Collins <collijk@uw.edu>
2020-03-31 01:28:24 -0700

    Baseline treatment component

--- src/vivarium_csu_zenon/components/treatment/parameters.py
+++ src/vivarium_csu_zenon/components/treatment/parameters.py
@@ -8,6 +8,9 @@ from vivarium_csu_zenon import paths
 from vivarium_csu_zenon.utilities import sample_truncnorm_distribution
 
 
+HIGH_LDL_BASELINE = 4.9
+PROB_LOW_DOSE = 0.5
+
 LOCATION_COLUMN = 'location'
 MEAN_COLUMN = 'mean_value'
 SD_COLUMN = 'sd_value'
@@ -15,6 +18,15 @@ SD_COLUMN = 'sd_value'
 INCREASE_DOSE = 'increasing_dose'
 ADD_SECOND_DRUG = 'adding_2nd_drug'
 SWITCH_DRUG = 'switching_drugs'
+
+MONOTHERAPY = 'monotherapy'
+FDC = 'fdc'
+
+EZETIMIBE = 'ezetimibe'
+FIBRATES = 'fibrates'
+STATIN_HIGH = 'high_potency_statin'
+STATIN_LOW = 'low_potency_statin'
+LIFESTYLE = 'lifestyle_intervention'
 
 
 def sample_probability_testing_ldl_c(location: str, draw: int) -> float:
@@ -64,7 +76,15 @@ def sample_raw_drug_prescription(location: str, draw: int, drug: str) -> float:
     """Raw result: needs to be adjusted"""
     seed = get_hash(f'{drug}_prescription_probability_draw_{draw}_location_{location}')
     data = pd.read_csv(paths.CURRENT_RX_DATA_PATH).set_index([LOCATION_COLUMN, 'current_prescription'])
-    params = data.loc[(location, drug), :]
+    params = data.loc[(location, drug.replace('_', ' ')), :]
+    return sample_truncnorm_distribution(seed, params[MEAN_COLUMN], params[SD_COLUMN])
+
+
+def sample_therapy_type(location: str, draw: int, therapy_type: str) -> float:
+    therapy_type = therapy_type.upper() if therapy_type is FDC else therapy_type
+    seed = get_hash(f'{therapy_type}_probability_draw_{draw}_location_{location}')
+    data = pd.read_csv(paths.PROB_THERAPY_TYPE).set_index([LOCATION_COLUMN, 'therapy_type'])
+    params = data.loc[(location, therapy_type), :]
     return sample_truncnorm_distribution(seed, params[MEAN_COLUMN], params[SD_COLUMN])
 
 
---
+++ src/vivarium_csu_zenon/components/treatment/treatment_coverage.py
@@ -0,0 +1,148 @@
+import typing
+
+import pandas as pd
+
+from vivarium_csu_zenon import globals as project_globals
+from vivarium_csu_zenon.components.treatment import parameters
+
+if typing.TYPE_CHECKING:
+    from vivarium.framework.engine import Builder
+    from vivarium.framework.population import SimulantData
+
+
+# NOTE:
+# `bad_ldlc` is defined here as a person's untreated ldlc is above the
+# ldlc threshold, regardless of what their actual ldlc level is.
+
+class LDLCTreatmentCoverage:
+
+    @property
+    def name(self):
+        """The name of this component."""
+        return 'ldlc_treatment_coverage'
+
+    # noinspection PyAttributeOutsideInit
+    def setup(self, builder: 'Builder'):
+        """Perform this component's setup."""
+        self.randomness = builder.randomness.get_stream(self.name)
+
+        self.ldlc = builder.value.get_value('high_ldl_cholesterol.exposure')
+
+        self.p_tx_given_bad_ldlc, self.p_target_given_treated = self.load_treatment_and_target_p(builder)
+        self.p_therapy_type = self.load_therapy_type_p(builder)
+        self.p_treatment_type = self.load_treatment_type_p(builder)
+
+        columns_created = [parameters.STATIN_HIGH, parameters.STATIN_LOW, parameters.EZETIMIBE,
+                           parameters.FIBRATES, parameters.LIFESTYLE, parameters.FDC]
+        self.population_view = builder.population.get_view(columns_created)
+        builder.population.initializes_simulants(self.on_initialize_simulants,
+                                                 creates_columns=columns_created,
+                                                 requires_columns=[project_globals.ISCHEMIC_STROKE_MODEL_NAME,
+                                                                   project_globals.IHD_MODEL_NAME],
+                                                 requires_values=['high_ldl_cholesterol.exposure'],
+                                                 requires_streams=[self.name])
+
+    def on_initialize_simulants(self, pop_data: 'SimulantData'):
+        """Initialize treatment status"""
+        pop_update = pd.DataFrame({
+            parameters.STATIN_HIGH: 'none',  # 'none', 'low', 'high'
+            parameters.STATIN_LOW: 'none',  # 'none', 'low', 'high'
+            parameters.EZETIMIBE: False,
+            parameters.FIBRATES: False,
+            parameters.LIFESTYLE: False,
+            parameters.FDC: False,
+        })
+
+        ldlc = self.ldlc(pop_data.index)
+        treatment_probability = self.get_treatment_probability(ldlc)
+        treated = self.randomness.get_draw(pop_data.index) < treatment_probability
+        mono_if_treated = (self.randomness.get_draw(pop_data.index, additional_key='therapy_type')
+                           < self.p_therapy_type[parameters.MONOTHERAPY])
+        treatment_type_if_mono = self.randomness.choice(pop_data.index,
+                                                        list(self.p_treatment_type.keys()),
+                                                        list(self.p_treatment_type.values()),
+                                                        additional_key='treatment_type')
+        ezetimibe_if_mono = treatment_type_if_mono == parameters.EZETIMIBE
+        fibrates_if_mono = treatment_type_if_mono == parameters.FIBRATES
+        low_statin_if_mono = treatment_type_if_mono == parameters.STATIN_LOW
+        high_statin_if_mono = treatment_type_if_mono == parameters.STATIN_HIGH
+        fdc_if_multi = (self.randomness.get_draw(pop_data.index, additional_key='fdc')
+                        < self.p_therapy_type[parameters.FDC])
+        p_low = (self.p_treatment_type[parameters.STATIN_LOW]
+                 / (self.p_treatment_type[parameters.STATIN_LOW] + self.p_treatment_type[parameters.STATIN_HIGH]))
+        # noinspection PyTypeChecker
+        low_potency_statin_if_not_fdc = (self.randomness.get_draw(pop_data.index, additional_key='low_potency_statin')
+                                         < p_low)
+        low_dose_if_statin = (self.randomness.get_draw(pop_data.index, additional_key='statin_dose')
+                              < parameters.PROB_LOW_DOSE)
+
+        # potency_statin_dose
+        high_statin = ((treated & mono_if_treated & high_statin_if_mono)
+                       | (treated & ~mono_if_treated & ~fdc_if_multi & ~low_potency_statin_if_not_fdc))
+        low_statin = ((treated & mono_if_treated & low_statin_if_mono)
+                      | (treated & ~mono_if_treated & ~fdc_if_multi & low_potency_statin_if_not_fdc))
+        # potency_statin_dose
+        high_statin_high = high_statin & ~low_dose_if_statin
+        high_statin_low = high_statin & low_dose_if_statin
+        low_statin_high = low_statin & ~low_dose_if_statin
+        low_statin_low = low_statin & low_dose_if_statin
+
+        ezetimibe = ((treated & mono_if_treated & ezetimibe_if_mono)
+                     | (treated & ~mono_if_treated))
+
+        fibrates = treated & mono_if_treated & fibrates_if_mono
+
+        fdc = treated & ~mono_if_treated & fdc_if_multi
+
+        pop_update.loc[high_statin_high, parameters.STATIN_HIGH] = 'high'
+        pop_update.loc[high_statin_low, parameters.STATIN_HIGH] = 'low'
+        pop_update.loc[low_statin_high, parameters.STATIN_LOW] = 'high'
+        pop_update.loc[low_statin_low, parameters.STATIN_LOW] = 'low'
+        pop_update.loc[ezetimibe, parameters.EZETIMIBE] = True
+        pop_update.loc[fibrates, parameters.FIBRATES] = True
+        pop_update.loc[fdc, parameters.FDC] = True
+        self.population_view.update(pop_update)
+
+    def get_treatment_probability(self, ldlc):
+        high_ldlc = ldlc > parameters.HIGH_LDL_BASELINE
+        # FIXME: Generate data, this is an awful hack for small sample sizes and not age/sex specific.
+        p_high_ldlc = len(ldlc[ldlc > parameters.HIGH_LDL_BASELINE])/len(ldlc)
+        p_bad_ldlc = p_high_ldlc / (1 - self.p_target_given_treated * self.p_tx_given_bad_ldlc)
+
+        p_treated_low = (self.p_target_given_treated * self.p_tx_given_bad_ldlc * p_bad_ldlc
+                         / (1 - p_high_ldlc))
+        p_treated_high = ((1 - self.p_target_given_treated) * self.p_tx_given_bad_ldlc * p_bad_ldlc
+                          / p_high_ldlc)
+        treatment_probability = pd.Series(p_treated_low, index=ldlc.index)
+        treatment_probability.loc[high_ldlc] = p_treated_high
+        return treatment_probability
+
+    @staticmethod
+    def load_treatment_and_target_p(builder):
+        location = builder.configuration.input_data.location
+        draw = builder.configuration.input_data.input_draw_number
+        p_tx_given_bad_ldlc = parameters.sample_probability_rx_given_high_ldl_c(location, draw)
+        p_target_among_treated = parameters.sample_probability_target_given_rx(location, draw)
+        return p_tx_given_bad_ldlc, p_target_among_treated
+
+    @staticmethod
+    def load_therapy_type_p(builder):
+        location = builder.configuration.input_data.location
+        draw = builder.configuration.input_data.input_draw_number
+        p_therapy_type = {therapy_type: parameters.sample_therapy_type(location, draw, therapy_type)
+                          for therapy_type in [parameters.MONOTHERAPY, parameters.FDC]}
+        return p_therapy_type
+
+    @staticmethod
+    def load_treatment_type_p(builder):
+        location = builder.configuration.input_data.location
+        draw = builder.configuration.input_data.input_draw_number
+        p_treatment_type = {treatment_type: parameters.sample_raw_drug_prescription(location, draw, treatment_type)
+                            for treatment_type in [parameters.EZETIMIBE, parameters.FIBRATES,
+                                                   parameters.STATIN_HIGH, parameters.STATIN_LOW]}
+        p_treatment_type = dict(zip([k for k in p_treatment_type.keys()],
+                                    parameters.get_adjusted_probabilities(*p_treatment_type.values())))
+        return p_treatment_type
+
+
+
