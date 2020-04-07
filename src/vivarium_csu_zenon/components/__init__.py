b53a9d5b8ed11077121d3f1eac325cc355532050
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-04-06 21:21:19 -0700

    first commit of observers

--- src/vivarium_csu_zenon/components/__init__.py
+++ src/vivarium_csu_zenon/components/__init__.py
@@ -1,5 +1,5 @@
 from .disease import IschemicHeartDisease, IschemicStroke, DiabetesMellitus, ChronicKidneyDisease
-from .observers import DiseaseObserver, MortalityObserver, DisabilityObserver
+from .observers import DiseaseObserver, MortalityObserver, DisabilityObserver, MiscellaneousObserver
 from .risk import RiskEffect, FastingPlasmaGlucose, Risk, LDLCholesterolRisk
 from .cvd import CVDRiskAttribute
 from .correlation import CorrelatedPropensityGenerator
